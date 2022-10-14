7212398b6fe5b398ef8157ba772cd796315a8d06
SachinMulge18 <sachinmulge98@gmail.com>
2022-10-14 11:44:54 +0530

    es

---
+++ src/components/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+        <h1>Sign In </h1>
+        <label>E-Mail</lable>
+        <in
+    </div>
+  )
+}
+
+export default LoginPage
